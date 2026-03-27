e1ef476a63a9795addd3b98006d2de9b54b48591
sucesso123 <walkerque300@gmail.com>
2026-03-27 17:14:01 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { MessageCircle, FileText, Wrench, MoreHorizontal, Phone } from 'lucide-react';
 
 function App() {
-  const whatsappNumber = '5511957658784';
+  const whatsappNumber = '5511958228366';
 
   const openWhatsApp = (message: string) => {
     const encodedMessage = encodeURIComponent(message);
